e72f63808f49019b33dfae6d69529accc89bcd0b
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2024-02-21 01:20:02 +0200

    also bush

--- FrontEnd/computershop/src/components/navbar/navbar.jsx
+++ FrontEnd/computershop/src/components/navbar/navbar.jsx
@@ -1,22 +1,38 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import logo from '../../assets/techtrove.png';
 import './navbar.css';
 import { useAuthContext } from '../../hooks/useAuthContext';
-import jwt_decode from 'jwt-decode';
+
 const Navbar = () => {
     const { user } = useAuthContext();
-    console.log(user)
-    const username = user.username
+    const username = user ? user.username : "Sign In";
+    const image = user ? `http://localhost:5000/uploads/${user.image}` : logo 
+
+    useEffect(() => {
+        if (user) {
+            console.log(user);
+        }
+    }, [user]);
+
     return (
-        <div className='Navbar-main d-flex align-items-center bg-black'> 
-            <div className="w-100 d-flex align-items-center mt-3">
-                <img src={logo} alt="logo" className='Navbar-logo'/>
-                <h1 className='Navbar-heading'>TechTrove</h1>
-                <h1 className='Navbar-UserName'>.com</h1>
+        <div className='Navbar-main bg-black'>
+            <div className='NavBar-MainContainer d-flex align-items-center bg-black'>
+                <div className="w-100 d-flex align-items-center mt-3">
+                    <img src={logo} alt="logo" className='Navbar-logo'/>
+                    <p className='Navbar-heading'>TechTrove</p>
+                </div>
+                <div className='Navbar-UserNameandImage '>
+                    <button className='d-flex align-items-center bg-black Navbar-UserNameandImageButton'>
+                        <p className='Navbar-UserName'>username</p>
+                        <img src={image} alt="logo" className="Navbar-UserImage"></img>
+                    </button>
+                </div>
             </div>
-        
+            <div className="Navbar-NavbarRed d-flex align-items-center mt-3 justify-content-center">
+                <p className='Navbar-UserName'>kousa</p>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
