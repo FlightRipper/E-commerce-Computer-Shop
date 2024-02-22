09332ba56e20127ec5f1a2df06f6c892f6d983df
FlightRipper <101296159+FlightRipper@users.noreply.github.com>
2024-02-22 11:47:19 +0200

    g

--- FrontEnd/computershop/src/pages/homepage/homepage.jsx
+++ FrontEnd/computershop/src/pages/homepage/homepage.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import redpc from '../../assets/redpc.jpg';
+import secondpc from '../../assets/pc2.jpg';
 import './homepage.css';
 import Navbar from '../../components/navbar/navbar';
 import FeaturedCard from '../../components/feauturedCard/featuredCard';
+import Carousel from "react-bootstrap/Carousel";
+import { useNavigate } from 'react-router-dom';
+import Footer from '../../components/footer/footer';
 
 const HomePage = () => {
-    const [featuredProducts, setFeaturedProducts] = useState([]); // Corrected variable name
 
-    const getFeaturedProducts = async () => { // Corrected function name
+    const [featuredProducts, setFeaturedProducts] = useState([]);
+    const navigate = useNavigate();
+
+    const getFeaturedProducts = async () => {
         const response = await axios.get('http://localhost:5000/products/featured');
 
-        if (response.status === 200) {
+        if (response.status ===  200) {
             setFeaturedProducts(response.data);
         }
         console.log(response.data)
@@ -25,28 +31,40 @@ const HomePage = () => {
         <>
             <Navbar />
             <div className='HomePageMain'>
-                <img src={redpc} alt="" className='RedPC' />
+                <div className='RedPC'>
+                    <Carousel
+                        controls={false}
+                        indicators={false}
+                        interval={2000}
+                    >
+                        {[redpc, secondpc].map((image, index) => (
+                            <Carousel.Item key={index} className='carousel-item'>
+                                <img
+                                    src={image}
+                                    alt={`Slide ${index +  1}`}
+                                    className='carousel-image'
+                                />
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </div>
                 <p className='HomePageHeading'>Featured Products</p>
                 <div className='HomePageFeauturedProducts'>
                     {featuredProducts.map((product) => (
                         <button style={{border: 'none', outline: 'none', background: 'none'}}>
-                        <FeaturedCard
-                            image={product.image}
-                            price={product.price}
-                            title={product.name}
-                            description={product.description}
-                        />
+                            <FeaturedCard
+                                image={product.image}
+                                price={product.price}
+                                title={product.name}
+                                description={product.description}
+                            />
                         </button>
                     ))}
                 </div>
-                <div className='HomePageFooter'>
-                    <p className='HomePageFooterText'>© 2023 TechTrove. All rights reserved.</p>
-                    <p className='HomePageFooterText'>Privacy Policy | Terms of Service</p>
-                    <p className='HomePageFooterText'>Powered by TechTrove</p>
-                </div>
+                <Footer/>
+
             </div>
         </>
-        
     )
 }
 
