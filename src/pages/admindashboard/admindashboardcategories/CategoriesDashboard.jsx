f2d328e27e23f09a9397da5e1d91e3fc3b52d4d7
Adelshkeir <adelshkeir12@gmail.com>
2024-09-07 15:23:03 +0300

    added categories and sub categories pages

--- src/pages/admindashboard/admindashboardcategories/CategoriesDashboard.jsx
+++ src/pages/admindashboard/admindashboardcategories/CategoriesDashboard.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import Adminsidebar from '../../../components/adminnavbar/adminnavbar';
+import "./categoriesdashboard.css";
+import React from "react";
+import Adminsidebar from "../../../components/adminnavbar/adminnavbar";
+
 const CategoriesDashboard = () => {
   return (
     <div className="dashboard-content">
-      <Adminsidebar/>
-
+      <Adminsidebar />
+      <div className="dashboard-categories-main">
+        <div className="categories-table-container">
+          <table class="responstable">
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>CREATED AT</th>
+              <th>OPTIONS</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Foo</td>
+              <td>01/01/1978</td>
+              <td className="options-cell">
+                <div />
+                <button class="button">
+                  <span class="button-content">Edit </span>
+                </button>
+                <div />
+                <button class="button delete-button">
+                  <span class="button-content">Delete </span>
+                </button>
+                <div />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/admindashboard/admindashboardsubcategories/Subcategoriesdashboard.jsx
+++ src/pages/admindashboard/admindashboardsubcategories/Subcategoriesdashboard.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import Adminsidebar from '../../../components/adminnavbar/adminnavbar';
+import './subcategoriesdashboard.css'
 const SubcategoriesDashboard = () => {
   return (
     <div className="dashboard-content">
-      <Adminsidebar/>
+      <Adminsidebar />
+      <div className="dashboard-categories-main">
+        <div className="categories-table-container">
+          <table class="responstable">
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>CATEGORY</th>
+              <th>CREATED AT</th>
+              <th>OPTIONS</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Foo</td>
+              <td>A&&A</td>
+              <td>01/01/1978</td>
+              <td className="options-cell">
+                <div />
+                <button class="button">
+                  <span class="button-content">Edit </span>
+                </button>
+                <div />
+                <button class="button delete-button">
+                  <span class="button-content">Delete </span>
+                </button>
+                <div />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
