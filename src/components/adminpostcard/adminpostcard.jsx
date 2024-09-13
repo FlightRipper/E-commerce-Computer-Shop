c6ee0d69715c743ef09f23f320f2bce12f7b50fd
Adelshkeir <adelshkeir12@gmail.com>
2024-09-13 17:29:38 +0300

    nice

--- src/components/adminpostcard/adminpostcard.jsx
+++ src/components/adminpostcard/adminpostcard.jsx
@@ -1,13 +1,21 @@
 import "./adminpostcard.css";
-const Adminpostcard = () => {
+
+const Adminpostcard = ({ title, description, imageUrl, dateCreated }) => {
   return (
-    <div class="product-admin-card">
-      <div class="product-admin-card-content">
-        <div class="product-admin-card-image">
-          <img src="https://cdn.discordapp.com/attachments/1146057681736581191/1280130262075834450/0_bhCQt7Hnc21Saxg7.png?ex=66daea36&is=66d998b6&hm=36886a56031a834802c682d3df2b1e82b70f0ccd9a6297582ff7561215aacd94&" />
+    <div className="product-admin-card">
+      <div className="product-admin-card-content">
+        <div className="product-admin-card-image">
+          <img
+            src={imageUrl}
+            alt={`Post Image`}
+            className="product-admin-card-image-img"
+          />
         </div>
-        <div class="product-admin-card-title">post</div>
-        <div class="product-admin-card-description">here is za description</div>
+        <div className="product-admin-card-description">
+          Created on: {dateCreated}
+        </div>
+
+        <div className="product-admin-card-date-created">{description}</div>
       </div>
 
       <div className="product-admin-card-button-container">
--- src/pages/admindashboard/admindashboardposts/PostsDashboard.jsx
+++ src/pages/admindashboard/admindashboardposts/PostsDashboard.jsx
@@ -1,68 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Adminsidebar from "../../../components/adminnavbar/adminnavbar";
 import Adminproductcard from "../../../components/adminproductcard/adminproductcard";
 import Adminpostcard from "../../../components/adminpostcard/adminpostcard";
+import axios from "axios";
+
 const PostsDashboard = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/posts");
+      if (response.status === 200) {
+        setPosts(response.data);
+        console.log(response.data)
+      } else {
+        console.error("Failed to fetch posts");
+      }
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <div className="dashboard-content-products">
       <Adminsidebar />
 
       <div className="dashboard-products-main">
         <div className="dashboard-products-cards-container">
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
-          <div className="dashboard-products-single-card">
-            <Adminpostcard />
-          </div>
+          {posts.map((post) => (
+            <div key={post.id} className="dashboard-products-single-card">
+              <Adminpostcard
+                description={post.description}
+                imageUrl={post.imageUrl}
+                dateCreated={post.createdAt}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
